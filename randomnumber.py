f2a766409dadde2dc801b676611d1d07330facbc
alenastepina <154559320+kowa541@users.noreply.github.com>
2024-11-28 19:42:33 +0500

    add choiceuser def

--- randomnumber.py
+++ randomnumber.py
@@ -13,3 +13,11 @@ def number_generation(level):
         attempts = 6
         st, end = 1, 50
     return number, attempts, st, end
+    def defchoiceuser():
+    choiceuser=None
+    while choiceuser not in (0, 1):
+        try:
+            choiceuser=int(input('1-да 0-нет '))
+        except:
+            print('вводите 0 или 1')
+    return choiceuser
